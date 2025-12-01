00d85ed676fb28a9080e9c67a8a8e7d6af41bc41
Chris Montrois <cmontrois@slack-corp.com>
2025-12-01 09:18:43 -0700

    Add import plugin to help while tsgo doesn't support it

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -386,6 +386,26 @@ return {
 		},
 	},
 
+	-- Import autocomplete
+	{
+		'piersolenski/import.nvim',
+		dependencies = {
+			'ibhagwan/fzf-lua',
+		},
+		opts = {
+			picker = "fzf-lua",
+		},
+		keys = {
+			{
+				"<leader>i",
+				function()
+					require("import").pick()
+				end,
+				desc = "Import",
+			},
+		},
+	},
+
 	------------------------------------------------------------------------------
 	-- LSP
 	------------------------------------------------------------------------------
